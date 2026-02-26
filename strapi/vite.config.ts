477a409c65c123d01328abe977b95060cb5b7a8f
MaxPunzMaxPunz <162301249+MaxPunzMaxPunz@users.noreply.github.com>
2026-02-26 09:56:18 +0100

    Update vite.config.ts

--- strapi/vite.config.ts
+++ strapi/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    allowedHosts: [process.env.SERVER ?? "localhost"],
+    allowedHosts: ["care4skillsback.duckdns.org"],
   },
 });
