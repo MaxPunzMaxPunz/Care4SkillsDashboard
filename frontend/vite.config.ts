4a8e8ac6b469f2418a7161fa85ebbfa850ba1dc7
MaxPunzMaxPunz <162301249+MaxPunzMaxPunz@users.noreply.github.com>
2026-02-26 12:23:02 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/postcss";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    allowedHosts: ["care4skillsdash.duckdns.org"], // your frontend domain
     proxy: {
       "/api": {
         target: "https://care4skillsback.duckdns.org",
