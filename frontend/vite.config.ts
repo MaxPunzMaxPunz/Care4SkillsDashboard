932d1a190e23d2d16037eae5835d8053221d850b
MaxPunzMaxPunz <162301249+MaxPunzMaxPunz@users.noreply.github.com>
2026-02-26 12:12:16 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from "@tailwindcss/postcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ["care4skillsfront.duckdns.org"], // your frontend domain
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
